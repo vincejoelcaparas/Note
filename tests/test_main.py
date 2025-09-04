a102691c153e30ff0943c8349aa4ebd1e8e4d989
vincejoelcaparas <caparasvince10@gmail.com>
2025-09-04 17:08:19 +0800

    fixing linting

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,12 @@
-import json
 from app.main import app
+
 
 def test_create_note():
     client = app.test_client()
     response = client.post("/notes", json={"text": "Learn DevOps"})
     assert response.status_code == 201
     assert response.get_json()["text"] == "Learn DevOps"
+
 
 def test_get_notes():
     client = app.test_client()
